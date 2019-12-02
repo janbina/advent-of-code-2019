3c89f9cb07bbff825c14ce65b93e778186405ac9
Jan Bína <binajohny@gmail.com>
2019-12-02 14:18:50 +0100

    Create template file

---
+++ template.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"aoc19/utils"
+)
+
+func main() {
+	part1()
+	part2()
+}
+
+func getInput() []int {
+	lines := utils.ReadLines("input.txt")
+	// strings := strings.Split(lines[0], ",")
+	// utils.StringsToInts(strings)
+}
+
+func part1() {
+	input := getInput()
+}
+
+func part2() {
+	input := getInput()
+}
