24816853539548695ed71018dbba6108c00f8409
Jan Bína <binajohny@gmail.com>
2019-12-02 12:56:17 +0100

    Day 2 simple solution

---
+++ day02/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"aoc19/utils"
+	"fmt"
+)
+
+func main() {
+	part1()
+	part2()
+}
+
+func part1() {
+	ints := utils.ReadNumbers2("input.txt")
+
+	out := runProgram(ints, 12, 2)
+
+	fmt.Println("Output =", out)
+}
+
+func part2() {
+	for noun := 0; noun < 100; noun++ {
+		for verb := 0; verb < 100; verb++ {
+			ints := utils.ReadNumbers2("input.txt")
+			out := runProgram(ints, noun, verb)
+			if out == 19690720 {
+				fmt.Println("Noun =", noun, "verb =", verb, "answer =", noun*100+verb)
+				return
+			}
+		}
+	}
+}
+
+func runProgram(mem []int, noun int, verb int) int {
+	mem[1] = noun
+	mem[2] = verb
+	position := 0
+
+	for {
+		command := mem[position]
+		if command == 99 {
+			break
+		} else if command == 1 {
+			mem[mem[position+3]] = mem[mem[position+1]] + mem[mem[position+2]]
+		} else if command == 2 {
+			mem[mem[position+3]] = mem[mem[position+1]] * mem[mem[position+2]]
+		}
+		position += 4
+	}
+
+	return mem[0]
+}
--- utils/input.go
+++ utils/input.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func ReadNumbers(filename string) []int {
@@ -17,6 +18,25 @@ func ReadNumbers(filename string) []int {
 	var numbers []int
 	for scanner.Scan() {
 		numbers = append(numbers, ToInt(scanner.Text()))
+	}
+	return numbers
+}
+
+func ReadNumbers2(filename string) []int {
+	file, err := os.Open(filename)
+	Check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	var numbers []int
+	for scanner.Scan() {
+		line := scanner.Text()
+		nums := strings.Split(line, ",")
+		for _, num := range nums {
+			numbers = append(numbers, ToInt(num))
+		}
 	}
 	return numbers
 }
