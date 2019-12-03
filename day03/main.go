82102c84356607c8cc417d9a09a14de9e4e66ae1
Jan Bína <binajohny@gmail.com>
2019-12-03 15:24:01 +0100

    Day 3 solution

---
+++ day03/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"aoc19/utils"
+	"fmt"
+	"math"
+	"strings"
+)
+
+type instruction struct {
+	dir byte
+	len int
+}
+
+type point struct {
+	x, y int
+}
+
+func main() {
+	part12()
+}
+
+func getInput() ([]instruction, []instruction) {
+	lines := utils.ReadLines("input.txt")
+	var inst1 []instruction
+	var inst2 []instruction
+
+	for _, inst := range strings.Split(lines[0], ",") {
+		inst1 = append(inst1, instruction{inst[0], utils.ToInt(inst[1:])})
+	}
+
+	for _, inst := range strings.Split(lines[1], ",") {
+		inst2 = append(inst2, instruction{inst[0], utils.ToInt(inst[1:])})
+	}
+
+	return inst1, inst2
+}
+
+func getPoints(inst []instruction) map[point]int {
+	m := make(map[point]int)
+
+	var x, y, steps int
+
+	d := map[byte]point{'U': {0, -1}, 'D': {0, 1}, 'L': {-1, 0}, 'R': {1, 0}}
+
+	for _, i := range inst {
+		for j := 0; j < i.len; j++ {
+			x += d[i.dir].x
+			y += d[i.dir].y
+			steps++
+			m[point{x, y}] = steps
+		}
+	}
+
+	return m
+}
+
+func part12() {
+	i1, i2 := getInput()
+	p1, p2 := getPoints(i1), getPoints(i2)
+
+	var minDist, minLen int = math.MaxInt32, math.MaxInt32
+
+	for k, v := range p1 {
+		if v2, ok := p2[k]; ok {
+			dist := utils.ManhattanDist(k.x, 0, k.y, 0)
+			minDist = utils.Min(minDist, dist)
+
+			len := v + v2
+			minLen = utils.Min(minLen, len)
+		}
+	}
+
+	fmt.Println("Part 1:", minDist)
+	fmt.Println("Part 2:", minLen)
+}
---
+++ utils/math.go
@@ -0,0 +1,32 @@
+package utils
+
+func Min(args ...int) int {
+	min := args[0]
+	for _, x := range args {
+		if x < min {
+			min = x
+		}
+	}
+	return min
+}
+
+func Max(args ...int) int {
+	max := args[0]
+	for _, x := range args {
+		if x > max {
+			max = x
+		}
+	}
+	return max
+}
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func ManhattanDist(x1 int, x2 int, y1 int, y2 int) int {
+	return Abs(x1-x2) + Abs(y1-y2)
+}
