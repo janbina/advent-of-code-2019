30ce861adc9aeac282f912aaef5ebd3f62a39e28
Jan Bína <binajohny@gmail.com>
2019-12-04 15:33:16 +0100

    Use string instead of int slice

--- day04/main.go
+++ day04/main.go
@@ -2,41 +2,16 @@ package main
 
 import "fmt"
 
-type code []int
-
 func main() {
 	part12()
 }
 
-func getInput() (code, code) {
-	return []int{1, 9, 7, 4, 8, 7}, []int{6, 7, 3, 2, 5, 1}
+func getInput() (int, int) {
+	return 197487, 673251
 }
 
-func (code code) inc() {
-	for i := len(code) - 1; i >= 0; i-- {
-		code[i] += 1
-		if code[i] == 10 {
-			code[i] = 0
-		} else {
-			break
-		}
-	}
-}
-
-func (code code) equals(code2 code) bool {
-	if len(code) != len(code2) {
-		return false
-	}
-	for i := 0; i < len(code); i++ {
-		if code[i] != code2[i] {
-			return false
-		}
-	}
-	return true
-}
-
-func (code code) isValidP1() bool {
-	prev := -1
+func isValidP1(code string) bool {
+	var prev byte = 0
 	flag := false
 	for i := 0; i < len(code); i++ {
 		if prev > code[i] {
@@ -49,8 +24,8 @@ func (code code) isValidP1() bool {
 	return flag
 }
 
-func (code code) isValidP2() bool {
-	prev := -1
+func isValidP2(code string) bool {
+	var prev byte = 0
 	flag := false
 	same := 1
 	for i := 0; i < len(code); i++ {
@@ -74,17 +49,20 @@ func (code code) isValidP2() bool {
 
 func part12() {
 	min, max := getInput()
+
 	p1Count := 0
 	p2Count := 0
-	for !min.equals(max) {
-		if min.isValidP1() {
+
+	for codeInt := min; codeInt <= max; codeInt++ {
+		code := fmt.Sprint(codeInt)
+		if isValidP1(code) {
 			p1Count++
 		}
-		if min.isValidP2() {
+		if isValidP2(code) {
 			p2Count++
 		}
-		min.inc()
 	}
+
 	fmt.Println(p1Count)
 	fmt.Println(p2Count)
 }
