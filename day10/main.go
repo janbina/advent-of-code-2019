ac4f12424608224b7bf55bfa1b3850848c70bed9
Jan Bína <binajohny@gmail.com>
2019-12-11 20:25:21 +0100

    Day 10 p2 solution

--- day10/main.go
+++ day10/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"aoc19/utils"
 	"fmt"
+	"math"
+	"sort"
 )
 
 func main() {
-	part1()
-	part2()
+	best, max := part1()
+	fmt.Println(max)
+	a200th := part2(best)
+	fmt.Println(a200th.y*100 + a200th.x)
 }
 
 type point struct {
@@ -27,7 +31,7 @@ func getInput() map[point]bool {
 	return asteroids
 }
 
-func part1() {
+func part1() (point, int) {
 	asteroids := getInput()
 	var asteroidsArr []point
 	for k := range asteroids {
@@ -55,7 +59,7 @@ func part1() {
 		}
 	}
 
-	fmt.Println(max, best.x, best.y)
+	return best, max
 }
 
 // returns true if there is an asteroid on the direct way between a and b
@@ -84,5 +88,61 @@ func isBlocked(asteroids map[point]bool, a, b point) bool {
 	}
 }
 
-func part2() {
+type helper struct {
+	angle    float64
+	len      int
+	asteroid point
+	order    int
+}
+
+func part2(best point) point {
+	asteroids := getInput()
+	delete(asteroids, best)
+	h := make([]helper, 0, len(asteroids))
+
+	for k := range asteroids {
+		vec := point{k.x - best.x, k.y - best.y}
+		len := utils.Abs(vec.x) + utils.Abs(vec.y)
+		angle := getAngle(float64(vec.x), float64(vec.y))
+		h = append(h, helper{angle, len, k, 0})
+	}
+
+	sort.Slice(h, func(i, j int) bool {
+		return h[i].angle < h[j].angle
+	})
+
+	cnt := 0
+	prev := -1000.0
+	for i, x := range h {
+		if x.angle == prev {
+			cnt++
+			h[i].order = cnt
+		} else {
+			cnt = 0
+			h[i].order = 0
+			prev = x.angle
+		}
+	}
+
+	sort.Slice(h, func(i, j int) bool {
+		if h[i].order == h[j].order {
+			return h[i].angle < h[j].angle
+		}
+		return h[i].order < h[j].order
+	})
+
+	return h[199].asteroid
+}
+
+func getAngle(x, y float64) float64 {
+	a := math.Atan2(x, y)
+	if a >= 0 {
+		return a
+	}
+	a += 2 * math.Pi
+
+	if a >= 3*math.Pi/2 {
+		a = a - 2*math.Pi
+	}
+	return a
 }
